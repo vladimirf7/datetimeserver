f5b3252b3bf482927331c4c82ba282d697c3c7ba
user <user@DESKTOP-D39G5JK>
2022-03-13 16:59:57 -0400

    Add 3 UDP request types

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import sys
 def send_udp_request(message, ip = '127.0.0.1', port = 8000):
     print(f'Sending "{message}" to {ip}:{port}')
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(5)
     sock.sendto(str.encode(message), (ip, port))
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print(f'received reply: {data}')
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+from datetime import datetime, timezone
 
 
 def start_server(port = 8000):
@@ -10,8 +11,13 @@ def start_server(port = 8000):
     while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         print(f'received message: {data}')
-        if data:
-            sock.sendto(data, addr)
+        if data.decode() == 'datetime':
+            sock.sendto(datetime.now(timezone.utc).isoformat().encode(), addr)
+        if data.decode() == 'date':
+            sock.sendto(datetime.now(timezone.utc).strftime('%Y-%m-%d').encode(), addr)
+        if data.decode() == 'time':
+            # import pdb; pdb.set_trace()
+            sock.sendto(datetime.now(timezone.utc).strftime('%H:%M:%S %Z').encode(), addr)
 
 
 if __name__ == "__main__":
